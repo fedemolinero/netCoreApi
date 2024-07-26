a3d214606e6feb16f59ad06d4f9eeb93aa56c55c
unknown <fmmolinero@gmail.com>
2024-07-26 01:22:03 -0300

    adding files for databases

--- PizzaStore/Pizza.cs
+++ PizzaStore/Pizza.cs
@@ -1,9 +1,17 @@
-namespace PizzaStore.Models 
+using Microsoft.EntityFrameworkCore;
+namespace PizzaStore.Models
 {
     public class Pizza
     {
-          public int Id { get; set; }
-          public string? Name { get; set; }
-          public string? Description { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
+
+    class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
+    }
+
 }
